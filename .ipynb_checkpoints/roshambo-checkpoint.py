4bdaf0846d6ecbb71aec6e4e860a2b13da4f6fa6
Alex Huynh <alexhuynh@Alexs-MacBook-Pro.local>
2024-01-13 21:03:45 -0800

    added dictionaries

--- .ipynb_checkpoints/roshambo-checkpoint.py
+++ .ipynb_checkpoints/roshambo-checkpoint.py
@@ -3,72 +3,73 @@ import random
 def roshambo():
     
     # define variables
-    choices = ["rock", "paper", "scissors"]
-    rules = {
-        "rock": "scissors",
-        "paper": "rock",
-        "scissors": "paper"
-    }
+    choices = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
+    choice_keys = {"r": "rock", "p": "paper", "s": "scissors"}
+    exit_commands = {'exit', 'end', 'forfeit', 'leave', 'quit', 'stop', 'surrender', 'e', 'l', 'f', 'q'}
+    tie_emoji, win_emoji, lose_emoji = "🤝", "🏆", "🤖"
     
     # display screen
     print("Welcome to Roshambo!")
-    print("exit, quit, stop, or leave at anytime \n")
-
+    print("Type 'exit', 'forfeit', quit', 'stop', 'surrender' or 'leave' at any time to stop the game.\n")
+    
     # set scoreboard
-    point_me, point_machines, stalemate = 0, 0, 0
+    scores = {"Player": 0, "CPU": 0, "Stalemate": 0}
     scoreboard = []
 
     while True: # start game of roshambo
-        user_input = input("Draw on scisssors...\n  Rock, Paper, Scissors\n\n    Shoot:").strip().lower()
+        user_input = input("Draw on shoot...\n  Rock, Paper, Scissors\n\n    Shoot:").strip().lower()
 
         # break: allow user to leave game
-        if user_input in ['exit', 'quit','forfeit', 'stop', 'surrender' 'leave', 'Exit', 'Quit', 'Forfeit', 'Stop', 'Surrender', 'Leave', 'e', 'q', 'f', 'l']:
+        if user_input in exit_commands:
             break
-
-        if user_input not in choices: # edge case: valid user input
+        
+        user_choice = choice_keys.get(user_input, user_input)
+            
+        if user_choice not in choices: # edge case: valid user input
             print("Stop cheating. Only Rock, Paper, or Scissors\nReady? ")
             continue
 
         # cpu's move
-        cpu_choice = random.choice(choices)
-        print(f"CPU chose: {cpu_choice.capitalize()}")
-
+        cpu_choice = random.choice(list(choices.keys()))
+        print(f"CPU chose: {cpu_choice.capitalize()}\n")
+        
         # who's the winner
-        if user_input == cpu_choice:
-            result = "It's a tie! 🤝"
-            stalemate += 1
-        elif rules[user_input] == cpu_choice:
-            result = "You win! 🏆"
-            point_me += 1
+        if user_choice == cpu_choice:
+            result, winner = "It's a tie! " + tie_emoji, "Stalemate"
+        
+        elif choices[user_choice] == cpu_choice:
+            result, winner = "You win! " + win_emoji, "Player"
+        
         else:
-            result = "You lose! 🤖"
-            point_machines += 1
-
+            result, winner = "You lose! "+ lose_emoji, "CPU"
             
         # round recap, update scoreboard
         print(f"\nRound Result: {result}")
-        scoreboard.append((user_input, cpu_choice))
-        print(f"Current Score: Team We - {point_me}, Machines - {point_machines}, Stalemates - {stalemate}\n")
-
+        scores[winner] += 1
+        scoreboard.append((user_choice, cpu_choice))
+        print(f"{result}\nCurrent Score: {scores}\n")
+        
     # game over, show final score and winner
-    print("\nAtlas! The Games have Concluded! \n")
-    print(f"Final Score: Team We - {point_me}, Machines - {point_machines}, Stalemate - {stalemate}")
+    print("\nAtlas! The Games have Concluded! \n", scores)
+    print("\nFinal Score:", scores)
+   
+    if scores["Player"] == scores["CPU"] or (scores["Stalemate"] > scores["Player"] and scores["Stalemate"] > scores["CPU"]):
+        print("Words of the world wistfully weave a wayward, worrisome waltz with a Stalemate between Humans and Machines! 🌬🎑🗞")
+        
+    elif scores["Player"] > scores["CPU"]:
+        print("Humans Prevail! 👩‍🎓🧑‍💻🕵️‍♂️")
     
-    if point_me > point_machines:
-        print("US HUMANS PREVAILED 👩‍🎓🧑‍💻🕵️‍♂️")
-    
-    elif point_me < point_machines:
-        print("THE MACHINES HAVE WON 🤖🦾🦿")
+    elif scores["Player"] < scores["CPU"]:
+        print("The Machines Win! 🤖🦾🦿")
     
     else:
-        print("Words of the world wistfully weave a wayward, worrisome waltz 🌬🎑🗞")
-    
-    print('\n') 
-    
+        print("Words of the world wistfully weave a wayward, worrisome waltz with a Stalemate between Humans and Machines! 🌬🎑🗞")
+
+
     # display recap of all the rounds
-    print(f"Round Recaps: ")
-    for index, (user, cpu) in enumerate(scoreboard, start=1):
-        print(f"Round {index}: You chose {user.capitalize()}, CPU chose {cpu.capitalize()}")
+    print(f"\nRound Recaps: ")
+    for index, (player, cpu) in enumerate(scoreboard, start=1):
+        print(f"Round {index}: You chose {player.capitalize()}, CPU chose {cpu.capitalize()}")
 
 ####    Main function    ####
 roshambo()
